b0b39389f76df3651c228e5f8568040c8ef1a915
whiteboii <captainwhiteboii@gmail.com>
2020-04-04 14:00:24 +0200

    adding a controller with a hardcoded response

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dispatch_system.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetUserById(int id)
+        {
+            var user = new Dictionary<String, string>();
+            user.Add(id.ToString(), "hardcoded response");
+            return Ok(user);
+        }
+    }
+}
